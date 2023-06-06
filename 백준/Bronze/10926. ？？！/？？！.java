737f39a5113ded311d56d0dbfbe09275753570c0
Joona5 <79864595+joona55@users.noreply.github.com>
2023-06-06 11:48:10 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        String name = sc.nextLine();
+
+        System.out.println(name + "??!");
+        //end line
+    }
+} 
