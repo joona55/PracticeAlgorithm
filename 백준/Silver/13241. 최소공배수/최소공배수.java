b95cf8df7de244adc3fa3ed41f4477ef99336b94
Joona5 <79864595+joona55@users.noreply.github.com>
2023-06-07 20:28:56 +0900

    [Silver V] Title: 최소공배수, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+
+        System.out.println(a * b / gcd(a, b));
+
+    }
+    public static long gcd(long a, long b) {
+        long c = 0;
+
+        while(b > 0) {
+            c = a % b;
+            a = b;
+            b = c;
+        }
+
+        return a;
+        /*
+         * 유클리드 호제법
+         * 1. 큰수를 작은수로 나누기
+         * 2. 나누는 수를 나머지로 계속 나누기
+         * 3. 나머지가 0이 나올면 그 때 나누는 수가 최대공약수
+         *  
+         * G = 최대공약수, R = 나머지, Q = 몫
+         * a = b * Q + R
+         * a * G = (b * Q * G) + r
+         * r = a * Q - (b * Q * G)
+         *   = G *(a - (b * Q))
+         * 
+         * 최소공배수 = (a * b) / 최대공약수
+         */
+    }
+} 
