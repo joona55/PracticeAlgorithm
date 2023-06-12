d7ffdaa3c8a89dcdd8f38490d3d466aa414f8264
Joona5 <79864595+joona55@users.noreply.github.com>
2023-06-13 00:51:14 +0900

    [Bronze IV] Title: 녹색거탑, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        System.out.println((int) Math.pow(2, n));
+        //end line
+    }
+} 
